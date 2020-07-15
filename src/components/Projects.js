8f8829cfe865d954bf89163c8ca4e7d23e83c83d
Gray Steakley <grayson.steakley@gmail.com>
2020-07-15 14:17:17 -0500

    Fixed the pics
    sdf

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,11 +18,11 @@ export default function Projects() {
                 <div className="card text-up-anim bg-dark text-white col-sm-6 col-12">
                         <a target = "_blank" href="https://merng-post-app.netlify.app/">
                         <div className="card-img-top overflow">
-                            <img src="./images/Post.png" alt="Portfolio App" className="card-img-top img-hover"/>
+                            <img src="./images/merng.gif" alt="Portfolio App" className="card-img-top img-hover"/>
                         </div>
                         </a>   
                         <div className="card-body">
-                            <h4 className="card-title">Post Site</h4>
+                            <h4 className="card-title">MERNG Post Site</h4>
                             <p className="card-text">
                             MERNG Stack, React, MongoDB, GraphQL, Express, Node.js, Semantic-UI, Context API
                             </p>
@@ -41,7 +41,7 @@ export default function Projects() {
                             </a>
                         </div>
                     </div>
-
+{/* 
                     
                     <div className="card text-up-anim2 bg-dark text-white col-sm-6 col-12 overflow">
                         <a href="https://react-tommys-pizza.netlify.app/" target="_blank">
@@ -56,6 +56,33 @@ export default function Projects() {
                             </p>
                             <p className="card-text">
                             Built a stylized pizza restaurant website that makes use of many custom CSS classes and animations, as well as modular React components. User Interface was examined to make a sleek and responsive design. a product API, stored into a separate Context API state. The Context API holds multiple functions that tabulates product pricing and which items are currently placed in the cart. 
+                            </p>
+                        </div>
+                        <div className="card-footer">
+                            <a target ="_blank" href="https://react-tommys-pizza.netlify.app/" className="btn btn-primary mr-5">
+                                Live Demo
+                            </a>
+                            <a target="_blank" href="https://github.com/graysonsteakley/React-Tommys-Pizza" className="btn btn-primary">
+                            GitHub <i class="fa fa-github" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div> */}
+
+                    
+                    <div className="card text-up-anim2 bg-dark text-white col-sm-6 col-12 overflow">
+                        <a href="https://oth-mockup.netlify.app/" target="_blank">
+                        <div className="card-img-top overflow">
+                        <img src="./images/mockup.gif" alt="Restaurant App" className="card-img-top img-hover"/>
+                        </div>
+                        </a>
+                        <div className="card-body">
+                            <h4 className="card-title">React SaaS Site</h4>
+                            <p className="card-text">
+                            React.js, Styled-Components, ReactStrap, Bootstrap, Semantic-UI, React-Router
+                            </p>
+                            <p className="card-text">
+                            Refactored a wordpress build with elementor site, into a functional React based site for responsiveness.
+                            Focused on user experience and ux/ui design.
                             </p>
                         </div>
                         <div className="card-footer">
