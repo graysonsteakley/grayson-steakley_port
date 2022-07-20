98232cf929a5ce53113da89ab8c0d4afc00e25bf
Grayson Steakley <71646562+Gray-Shuup@users.noreply.github.com>
2022-07-20 16:25:09 -0500

    chore: cleanup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
 import styled from "styled-components";
 import SVGIcon from "../utils/SVGIcon";
