11ed311fc4e00370ab2d38fabeaafc9ef9db4468
gsteakle <gsteakle@cisco.com>
2022-07-21 00:27:43 -0500

    chore: form attr

--- src/views/ContactMePage.js
+++ src/views/ContactMePage.js
@@ -1,6 +1,6 @@
 // import axios from "axios";
 import React from "react";
-import { Button, FormGroup, Label } from "reactstrap";
+import { FormGroup, Label } from "reactstrap";
 import {
   BaseContainer,
   HeadingText,
